1590065123b9162c62da73acf68238e48390cc53
rjsdn0006 <80525718+rjsdn0006@users.noreply.github.com>
2021-12-26 19:46:04 +0900

    -
    -

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,154 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li v-if="step == 1" @click="step++">Next</li>
+      <li v-if="step == 2" @click="publish;">발행</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container :게시물="게시물" :step="step" :url="url" @write="작성한글 = $event"/>
+
+  <button @click="more">더보기</button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input @change="upload" type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from "./components/Container.vue";
+import data from "./assets/data.js";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Container: Container,
+  },
+  data() {
+    return {
+      게시물: data,
+      더보기: 0,
+      step: 0,
+      url: "",
+      작성한글: '',
+    };
+  },
+  methods: {
+    more() {
+      axios
+        .get(`https://codingapple1.github.io/vue/more${this.더보기}.json`)
+        .then((result) => {
+          // get요청이 성공하면 parameter에 결과를 담아서 실행시켜줌
+          this.게시물.push(result.data);
+          this.더보기++;
+        });
+    },
+    upload(e) {
+      let 파일 = e.target.files;
+      this.url = URL.createObjectURL(파일[0]);
+      this.step = 1;
+    },
+    publish() {
+      let 내게시물 = {
+        name: "Kim Hyun",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: this.url,
+        likes: 36,
+        date: "May 15",
+        liked: false,
+        content: this.작성한글,
+        filter: "perpetua",
+      };
+      this.게시물.unshift(내게시물);
+      this.step = 0; 
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
   margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <!-- 저번시간에 만든거 -->
+    <div v-if="step == 0">
+      <Post :게시물="게시물[i]" v-for="(a, i) in 게시물" :key="i" />
+    </div>
+
+    <!-- 필터선택페이지 -->
+    <div v-if="step == 1">
+      <div class="upload-image" :style="{backgroundImage:`url('${url}')`}"></div>
+      <div class="filters">
+        <FilterBox :url="url" :filter="filter[i]" v-for="(item,i) in filter" :key="i"></FilterBox>
+      </div>
+    </div>
+
+    <!-- 글작성페이지 -->
+    <div v-if="step == 2">
+      <div class="upload-image" :style="{backgroundImage:`url('${url}')`}"></div>
+      <div class="write">
+        <textarea @input="$emit('write', $event.target.value)" class="write-box">write!</textarea>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+import FilterBox from "./FilterBox.vue";
+import filter from "../assets/filter.js";
+
+export default {
+  name: "Container",
+  data(){
+    return{
+      filter: filter,
+    }
+  },
+  components: {
+    Post: Post,
+    FilterBox: FilterBox,
+  },
+  props: {
+    게시물: Object,
+    step: Number,
+    url: String,
+  },
+};
+</script>
+
+<style>
+.upload-image {
+  width: 100%;
+  height: 450px;
+  background: cornflowerblue;
+  background-size: cover;
+}
+.filters {
+  overflow-x: scroll;
+  white-space: nowrap;
+}
+.filter-1 {
+  width: 100px;
+  height: 100px;
+  background-color: cornflowerblue;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+}
+.filters::-webkit-scrollbar {
+  height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+.write-box {
+  border: none;
+  width: 90%;
+  height: 100px;
+  padding: 15px;
+  margin: auto;
+  display: block;
+  outline: none;
+}
+</style>
